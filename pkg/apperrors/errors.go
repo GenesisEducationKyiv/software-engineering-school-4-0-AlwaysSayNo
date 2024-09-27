d8109d6a300f063e2002bd0ff24219d87d936f32
Admin <ML31feed>
2024-09-27 19:29:53 +0300

    [hw8-architecture-patterns] (Created a new module: apperrors)

--- pkg/apperrors/errors.go
+++ pkg/apperrors/errors.go
@@ -1,10 +1,32 @@
 package apperrors
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
 )
+
+// customError is used to create own types of apperrors and handle them appropriately.
+// It implements interface for standard error.
+type customError struct {
+	Message string
+	Cause   error
+}
+
+// Error returns formatted error message.
+func (e *customError) Error() string {
+	if e.Cause != nil && len(e.Message) != 0 {
+		return fmt.Sprintf("%s: %v", e.Message, e.Cause)
+	} else if e.Cause != nil {
+		return e.Cause.Error()
+	}
+	return e.Message
+}
+
+func (e *customError) Unwrap() error {
+	return e.Cause
+}
 
 type ValidationError struct {
 	customError
